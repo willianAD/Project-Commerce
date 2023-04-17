c74de61847c6f4942f6ed1c3db89f3ce88530732
willianAD <willian.afonso013@gmail.com>
2023-04-17 18:16:12 -0300

    Requisito 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,8 +1,7 @@
-// db.produtos.updateMany(
-//   {},
-//   { $set: { resumoProdutos: {
-//     franquia: "McDonalds",
-//     totalProdutos: db.produtos.countDocuments({}),
-//   } } },
-// );
-// db.produtos.find({}, { _id: 0, "resumoProdutos.franquia": 1, "resumoProdutos.totalProdutos": 1 });
+db.resumoProdutos.insertOne(
+  {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.countDocuments({}),
+  },
+);
+db.resumoProdutos.find({}, { _id: 0, franquia: 1, totalProdutos: 1 });
